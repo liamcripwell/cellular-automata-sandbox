6213408df730700c0340f810b66ca09b0b4714ca
liam <ljcripwell@gmail.com>
2019-01-03 19:01:13 +0100

    improvements made to grid generation and plotting

--- src/SvgPlot.elm
+++ src/SvgPlot.elm
@@ -2,17 +2,22 @@ import Html
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
+gridWidth = 50
+gridHeight = 50
+cellSize = 10
+
 plotSquares : List (Int, Int) -> Html.Html msg
 plotSquares =
   let
     toSquare (a, b) =
       rect
-        [ x (String.fromInt (a * 10))
-        , y (String.fromInt (100 - (b * 10)))
-        , width "10"
-        , height "10" ] []
+        [ x (String.fromInt (a * cellSize))
+        , y (String.fromInt ((gridHeight*cellSize) - (b * cellSize)))
+        , width <| String.fromInt cellSize
+        , height <| String.fromInt cellSize ] []
   in
-    svg [ width "100", height "100" ] << List.map toSquare
+    svg [ width <| String.fromInt (gridWidth*cellSize)
+        , height <| String.fromInt (gridHeight*cellSize) ] << List.map toSquare
 
 main =
-  plotSquares <| List.map (\x -> (x, x)) (List.range 1 9)
+  plotSquares <| List.map (\x -> (x, x)) (List.range 1 (gridWidth-1))
