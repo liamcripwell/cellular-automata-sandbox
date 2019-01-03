84bba5824a1936e92cacfba5c248369a8ef8cf9e
liam <ljcripwell@gmail.com>
2019-01-03 21:38:22 +0100

    better handling of non-square grid dimensions

--- src/SvgPlot.elm
+++ src/SvgPlot.elm
@@ -2,7 +2,7 @@ import Html
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
-gridWidth = 50
+gridWidth = 100
 gridHeight = 50
 cellSize = 10
 
@@ -19,5 +19,15 @@ plotSquares =
     svg [ width <| String.fromInt (gridWidth*cellSize)
         , height <| String.fromInt (gridHeight*cellSize) ] << List.map toSquare
 
+
+
+diagonalLine = List.map (\x -> (x, x)) (List.range 1 (gridWidth-1))
+
+perimeter = List.map (\x -> (1, x)) (List.range 1 (gridHeight-1)) 
+            ++ List.map (\x -> (x, 1)) (List.range 1 (gridWidth-1))
+            ++ List.map (\x -> (gridWidth-1, x)) (List.range 1 (gridHeight-1))
+            ++ List.map (\x -> (x, gridHeight-1)) (List.range 1 (gridWidth-1))
+
+
 main =
-  plotSquares <| List.map (\x -> (x, x)) (List.range 1 (gridWidth-1))
+  plotSquares <| perimeter
