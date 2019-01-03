f15693085601842806564b4b60d2401cbc331a54
liam <ljcripwell@gmail.com>
2019-01-03 22:04:13 +0100

    fixed grid reference so that coords are referenced from (0,0)

--- src/SvgPlot.elm
+++ src/SvgPlot.elm
@@ -12,7 +12,7 @@ plotSquares =
     toSquare (a, b) =
       rect
         [ x (String.fromInt (a * cellSize))
-        , y (String.fromInt ((gridHeight*cellSize) - (b * cellSize)))
+        , y (String.fromInt (b * cellSize))
         , width <| String.fromInt cellSize
         , height <| String.fromInt cellSize ] []
   in
@@ -21,12 +21,12 @@ plotSquares =
 
 
 
-diagonalLine = List.map (\x -> (x, x)) (List.range 1 (gridWidth-1))
+diagonalLine = List.map (\x -> (x, x)) (List.range 0 (gridWidth-1))
 
-perimeter = List.map (\x -> (1, x)) (List.range 1 (gridHeight-1)) 
-            ++ List.map (\x -> (x, 1)) (List.range 1 (gridWidth-1))
-            ++ List.map (\x -> (gridWidth-1, x)) (List.range 1 (gridHeight-1))
-            ++ List.map (\x -> (x, gridHeight-1)) (List.range 1 (gridWidth-1))
+perimeter = List.map (\x -> (0, x)) (List.range 0 (gridHeight-1)) 
+            ++ List.map (\x -> (x, 0)) (List.range 0 (gridWidth-1))
+            ++ List.map (\x -> (gridWidth-1, x)) (List.range 0 (gridHeight-1))
+            ++ List.map (\x -> (x, gridHeight-1)) (List.range 0 (gridWidth-1))
 
 
 main =
