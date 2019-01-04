b90e88b7cf1902abb19f2333b29063494e390efe
liam <ljcripwell@gmail.com>
2019-01-04 16:07:08 +0100

    adjusting scaling of cells in grid drawing

--- src/SvgPlot.elm
+++ src/SvgPlot.elm
@@ -3,8 +3,8 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 gridWidth = 7
-gridHeight = 10
-cellSize = 3
+gridHeight = 5
+cellSize = 19 -- Note: cell's internal size will be (cellSize-1)*(cellSize-1)
 
 plotSquares : List (Int, Int) -> Html.Html msg
 plotSquares =
@@ -20,29 +20,30 @@ plotSquares =
         , height <| String.fromInt (gridHeight*cellSize) ] << List.map toSquare
 
 
+drawGrid : Html.Html msg
 drawGrid = 
   let
       gridLineVert : Int -> Html.Html msg
       gridLineVert i =
         line
-          [ x1 (String.fromInt (i * cellSize +1))
-          , y1 "0"
-          , x2 (String.fromInt (i * cellSize +1))
-          , y2 <| String.fromInt <| (gridHeight*cellSize) +1
-          , stroke "black"
+          [ x1 (String.fromInt (i * cellSize + 1))
+          , y1 "1"
+          , x2 (String.fromInt (i * cellSize + 1))
+          , y2 <| String.fromInt <| (gridHeight*cellSize) + 1
+          , stroke "blue"
           , strokeWidth "1" ] []
       gridLineHori : Int -> Html.Html msg
       gridLineHori i =
         line
-          [ x1 "0"
+          [ x1 "1"
           , y1 (String.fromInt (i * cellSize + 1))
           , x2 <| String.fromInt <| (gridWidth*cellSize) + 1
-          , y2 (String.fromInt (i * cellSize +1))
-          , stroke "black"
+          , y2 (String.fromInt (i * cellSize + 1))
+          , stroke "blue"
           , strokeWidth "1" ] []
   in
-    svg [ width <| String.fromInt <| (gridWidth*cellSize) + (gridWidth+1)
-        , height <| String.fromInt <| (gridHeight*cellSize) + (gridHeight+1) 
+    svg [ width <| String.fromInt <| (gridWidth*(cellSize+1)) + 2
+        , height <| String.fromInt <| (gridHeight*(cellSize+1)) + 2
         ] 
         <| (List.map gridLineVert <| List.range 0 gridWidth)
           ++ (List.map gridLineHori <| List.range 0 gridHeight)
