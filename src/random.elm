367ecef149e90085703165eef00592a4c7f8c057
liam <ljcripwell@gmail.com>
2019-01-06 20:03:59 +0100

    added grid rendering to random.elm

--- src/random.elm
+++ src/random.elm
@@ -3,7 +3,8 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Random
 import Tuple
-
+import Svg 
+import Svg.Attributes exposing (..)
 
 
 -- MAIN
@@ -76,4 +77,57 @@ view model =
     [ h1 [] [ text (String.fromInt <| Tuple.first model.dieFace)
             , text (String.fromInt <| Tuple.second model.dieFace) ]
     , button [ onClick Roll ] [ text "Roll" ]
+    , br [] []
+    , drawGrid <| [(1, 1), (0, 2)]
     ]
+
+
+gridWidth = 50
+gridHeight = 35
+cellSize = 25 -- Note: cell's internal size will be (cellSize-1)*(cellSize-1)
+
+
+updateCells : List (Float, Float) -> Html.Html msg
+updateCells = 
+  let
+    toSquare (a, b) =
+      Svg.rect
+        [ x (String.fromFloat (a * cellSize + 1.5))
+        , y (String.fromFloat (b * cellSize + 1.5))
+        , width <| String.fromInt (cellSize - 1)
+        , height <| String.fromInt (cellSize - 1) 
+        , fill "purple" ] []
+  in
+    Svg.svg [ width <| String.fromInt <| (gridWidth*(cellSize+1)) + 2
+        , height <| String.fromInt <| (gridHeight*(cellSize+1)) + 2
+        ]  << List.map toSquare
+
+drawGrid : List (Float, Float) -> Html.Html msg
+drawGrid cells = 
+  let
+      gridLineVert : Int -> Svg.Svg msg
+      gridLineVert i =
+        Svg.line
+          [ x1 (String.fromInt (i * cellSize + 1))
+          , y1 "1"
+          , x2 (String.fromInt (i * cellSize + 1))
+          , y2 <| String.fromInt <| (gridHeight*cellSize) + 1
+          , stroke "lightgrey"
+          , strokeWidth "1" ] []
+      gridLineHori : Int -> Svg.Svg msg
+      gridLineHori i =
+        Svg.line
+          [ x1 "1"
+          , y1 (String.fromInt (i * cellSize + 1))
+          , x2 <| String.fromInt <| (gridWidth*cellSize) + 1
+          , y2 (String.fromInt (i * cellSize + 1))
+          , stroke "lightgrey"
+          , strokeWidth "1" ] []
+  in
+    Svg.svg [ width <| String.fromInt <| (gridWidth*(cellSize+1)) + 2
+        , height <| String.fromInt <| (gridHeight*(cellSize+1)) + 2
+        ] 
+        ([ List.map gridLineVert <| List.range 0 gridWidth
+         , List.map gridLineHori <| List.range 0 gridHeight
+         , [ updateCells cells ]
+         ] |> List.foldr (++) [])
