2075d9d772b5a8555052ee900373da7d85f25965
liam <ljcripwell@gmail.com>
2019-01-09 16:02:45 +0100

    moved current state of main program to Main.elm and deleted other two src files

--- src/Main.elm
+++ src/Main.elm
@@ -1,80 +1,212 @@
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (..)
+import Random
+import Tuple
+import Svg 
+import Svg.Attributes exposing (..)
+import List.Extra exposing (..)
+import Time
 
-import List exposing (map, range, sum, foldr, append)
 
-import Svg
-import Svg.Attributes as SvgA
+-- MAIN
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
 
 
 -- MODEL
 
-type alias Model = 
-  { rows : String
-  , cols : String
+
+type alias Model =
+  { liveCells : List (Int, Int)
+  , timeStep : Int
   }
 
-init : Model
-init =
-  Model "5" "5"
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model [(2+10,20),(3+10,20),(4+10,20),(5+10,20),(6+10,20),(7+10,20),(8+10,20),(9+10,20),(11+10,20),(12+10,20),(13+10,20),(14+10,20),(15+10,20),(19+10,20),(20+10,20),(21+10,20),(28+10,20),(29+10,20),(30+10,20),(31+10,20),(32+10,20),(33+10,20),(34+10,20),(36+10,20),(37+10,20),(38+10,20),(39+10,20),(40+10,20)] 0
+--   , Random.generate NewState <| randomCells 800
+  , Cmd.none
+  )
+
+
+ -- TODO: get rid of these global vars 
+gridWidth = 75
+gridHeight = 50
+cellSize = 10 -- Note: cell's internal size will be (cellSize-1)*(cellSize-1) 
+
 
 
 -- UPDATE
 
-type Msg 
-  = Rows String
-  | Cols String
 
-update : Msg -> Model -> Model
+type Msg
+  = Step Time.Posix
+  | NewState (List (Int, Int))
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Rows rows ->
-      { model | rows = rows }
+    Step newTime ->
+      ( { model | liveCells = 
+          (List.foldr (++) [] <| List.map (gameOfLife model.liveCells) model.liveCells)
+          ++ (List.foldr (++) [] <| List.map (gameOfDeath model.liveCells) <| cartesian (List.range 0 (gridWidth-1)) (List.range 0 (gridHeight-1)))
+         , timeStep = (model.timeStep + 1)}
+      , Cmd.none
+      )
 
-    Cols cols ->
-      { model | cols = cols }
+    NewState newLiveCells ->
+      ( { model | liveCells = newLiveCells }
+      , Cmd.none
+      )
+
+
+randomCell : Random.Generator (Int, Int)
+randomCell = Random.pair (Random.int 0 (gridWidth-1)) (Random.int 0 (gridHeight-1))
+
+randomCells : Int -> Random.Generator (List (Int, Int))
+randomCells n = Random.list n randomCell
+
+cartesian : List a -> List b -> List (a,b)
+cartesian xs ys =
+  List.concatMap
+    ( \x -> List.map ( \y -> (x, y) ) ys )
+    xs
+
+liveneighbors : List (Int, Int) -> (Int, Int) -> List (Int, Int)
+liveneighbors state (x, y) = 
+  cartesian (List.range (x-1) (x+1)) (List.range (y-1) (y+1))
+  |> List.filter (\a -> List.member a state)
+
+rule1 : Int -> (Int, Int) -> Maybe (Int, Int)
+rule1 neighborCount cell =
+  if neighborCount < 2 then Nothing else Just cell
+
+rule2 : Int -> (Int, Int) -> Maybe (Int, Int)
+rule2 neighborCount cell =
+  if neighborCount == 2 || neighborCount == 3 then Just cell else Nothing
+
+rule3 : Int -> (Int, Int) -> Maybe (Int, Int)
+rule3 neighborCount cell =
+  if neighborCount > 3 then Nothing else Just cell
+
+rule4 : Int -> (Int, Int) -> List (Int, Int)
+rule4 neighborCount cell =
+  if neighborCount == 3 then [cell] else []
+
+gameOfLife : List (Int, Int) -> (Int, Int) -> List (Int, Int)
+gameOfLife liveCells cell =
+  let
+    neighbors = liveneighbors liveCells cell
+    neighborCount = (List.length neighbors) - 1
+  in
+    case rule1 neighborCount cell of
+      Nothing -> []
+      Just x -> 
+        case rule2 neighborCount cell of
+          Nothing -> []
+          Just y ->
+            case rule3 neighborCount cell of
+                Nothing -> []
+                Just z -> [z]
+
+gameOfDeath : List (Int, Int) -> (Int, Int) -> List (Int, Int)
+gameOfDeath liveCells cell =
+    let
+        neighbors = liveneighbors liveCells cell
+        neighborCount = (List.length neighbors)
+        isLive = List.member cell liveCells
+    in
+      if isLive then []
+      else rule4 neighborCount cell
+
+
+type alias Automaton = 
+  { liveCells : List (Int, Int)
+  , deadCells : List (Int, Int)
+  , liveRules : List (Int -> (Int, Int) -> Maybe (Int, Int))
+  , deadRules : List (Int -> (Int, Int) -> Maybe (Int, Int))
+  }
+
+automataStep : Automaton -> Automaton
+automataStep automaton = 
+  { automaton | liveCells = automaton.liveCells } 
+
+-- TODO: perhaps implement a cell type and complete the automatonStep function above
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 100 Step
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
   div []
-    [ viewInput "text" "Rows" model.rows Rows
-    , viewInput "text" "Cols" model.cols Cols
-    , br [] []
-    , viewValidation model
-    , br [] []
-    , viewGrid model
+    [ drawGrid <| List.map (\x -> (toFloat <| Tuple.first x, toFloat <| Tuple.second x)) model.liveCells
+    , h1 [] [ text <| "Time Step: " ++ (String.fromInt model.timeStep) ]
     ]
 
-safeToInt = String.toInt >> Maybe.withDefault 0
 
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-  div [] 
-    [ text ("Number of " ++ p ++ ": ")
-    , input [ type_ t, placeholder p, value v, onInput toMsg ] [] ]
+updateCells : List (Float, Float) -> Html.Html msg
+updateCells = 
+  let
+    toSquare (a, b) =
+      Svg.rect
+        [ x (String.fromFloat (a * cellSize + 1.5))
+        , y (String.fromFloat (b * cellSize + 1.5))
+        , width <| String.fromInt (cellSize - 1)
+        , height <| String.fromInt (cellSize - 1) 
+        , fill "purple" ] []
+  in
+    Svg.svg [ width <| String.fromInt <| (gridWidth*(cellSize+1)) + 2
+        , height <| String.fromInt <| (gridHeight*(cellSize+1)) + 2
+        ]  << List.map toSquare
 
 
-viewValidation : Model -> Html msg
-viewValidation model =
-  div [] 
-    [ text ( "Grid Size: " ++ model.rows ++ "x" ++ model.cols) ]
+drawGrid : List (Float, Float) -> Html.Html msg
+drawGrid cells = 
+  let
+      gridLineVert : Int -> Svg.Svg msg
+      gridLineVert i =
+        Svg.line
+          [ x1 (String.fromInt (i * cellSize + 1))
+          , y1 "1"
+          , x2 (String.fromInt (i * cellSize + 1))
+          , y2 <| String.fromInt <| (gridHeight*cellSize) + 1
+          , stroke "lightgrey"
+          , strokeWidth "1" ] []
 
-
-
-viewGrid : Model -> Html msg
-viewGrid model =
-  div []
-    ( range 1 (safeToInt model.rows) |> map (\x ->
-        range 1 (safeToInt model.cols) |> map (\y -> 
-            input [ type_ "text" ] []) |> append [ br [] [] ]
-      ) |> foldr (++) [] )
-
+      gridLineHori : Int -> Svg.Svg msg
+      gridLineHori i =
+        Svg.line
+          [ x1 "1"
+          , y1 (String.fromInt (i * cellSize + 1))
+          , x2 <| String.fromInt <| (gridWidth*cellSize) + 1
+          , y2 (String.fromInt (i * cellSize + 1))
+          , stroke "lightgrey"
+          , strokeWidth "1" ] []
+  in
+    Svg.svg [ width <| String.fromInt <| (gridWidth*(cellSize+1)) + 2
+        , height <| String.fromInt <| (gridHeight*(cellSize+1)) + 2
+        ] 
+        ([ List.map gridLineVert <| List.range 0 gridWidth
+         , List.map gridLineHori <| List.range 0 gridHeight
+         , [ updateCells cells ]
+         ] |> List.foldr (++) [])
