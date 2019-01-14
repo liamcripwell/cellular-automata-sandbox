db4b8e766b1287bcd086f8afd040ce8c1d25f88e
liam <ljcripwell@gmail.com>
2019-01-14 16:50:41 +0100

    moved revised modelling code to seperate src

---
+++ src/Automata.elm
@@ -0,0 +1,59 @@
+import List.Extra exposing (..)
+
+cartesian : List a -> List b -> List (a,b)
+cartesian xs ys =
+  List.concatMap
+    ( \x -> List.map ( \y -> (x, y) ) ys )
+    xs
+
+type alias Automaton = 
+  { liveCells : List Cell
+  , deadCells : List Cell
+  , liveRules : List (Int -> Cell -> Maybe Cell)
+  , deadRules : List (Int -> Cell -> Maybe Cell)
+  }
+
+type alias Cell = (Int, Int)
+
+automataStep : Automaton -> Automaton
+automataStep automaton = 
+  { automaton | liveCells = automaton.liveCells
+              , deadCells = automaton.deadCells } 
+
+buildAutomaton : Int -> Int -> Int -> Automaton
+buildAutomaton width height cellDim = 
+  { liveCells = []
+  , deadCells = cartesian (List.range 0 (width-1)) (List.range 0 (height-1))
+  , liveRules = []
+  , deadRules = []
+  }
+  
+setCell : Cell -> Automaton -> Automaton
+setCell cell automaton  = 
+  { automaton | liveCells = cell :: automaton.liveCells
+              , deadCells = remove cell automaton.deadCells }
+
+killCell : Cell -> Automaton -> Automaton
+killCell cell automaton  = 
+  { automaton | liveCells = remove cell automaton.liveCells
+              , deadCells = cell :: automaton.deadCells }
+
+buildGameOfLife : Int -> Int -> Int -> Automaton
+buildGameOfLife width height cellDim = 
+  let
+      automaton = buildAutomaton width height cellDim
+      
+      r1 neighborCount cell =
+        if neighborCount < 2 then Nothing else Just cell
+
+      r2 neighborCount cell =
+        if neighborCount == 2 || neighborCount == 3 then Just cell else Nothing
+
+      r3 neighborCount cell =
+        if neighborCount > 3 then Nothing else Just cell
+
+      r4 neighborCount cell =
+        if neighborCount == 3 then Just cell else Nothing
+  in
+    { automaton | liveRules = [ r1, r2, r3 ]
+                , deadRules = [ r4 ] }
--- src/Main.elm
+++ src/Main.elm
@@ -146,58 +146,6 @@ gameOfDeath liveCells cell =
       else rule4 neighborCount cell
 
 
-type alias Automaton = 
-  { liveCells : List Cell
-  , deadCells : List Cell
-  , liveRules : List (Int -> Cell -> Maybe Cell)
-  , deadRules : List (Int -> Cell -> Maybe Cell)
-  }
-
-type alias Cell = (Int, Int)
-
-automataStep : Automaton -> Automaton
-automataStep automaton = 
-  { automaton | liveCells = automaton.liveCells
-              , deadCells = automaton.deadCells } 
-
-buildAutomaton : Int -> Int -> Int -> Automaton
-buildAutomaton width height cellDim = 
-  { liveCells = []
-  , deadCells = cartesian (List.range 0 (width-1)) (List.range 0 (height-1))
-  , liveRules = []
-  , deadRules = []
-  }
-  
-setCell : Cell -> Automaton -> Automaton
-setCell cell automaton  = 
-  { automaton | liveCells = cell :: automaton.liveCells
-              , deadCells = List.Extra.remove cell automaton.deadCells }
-
-killCell : Cell -> Automaton -> Automaton
-killCell cell automaton  = 
-  { automaton | liveCells = List.Extra.remove cell automaton.liveCells
-              , deadCells = cell :: automaton.deadCells }
-
-buildGameOfLife : Int -> Int -> Int -> Automaton
-buildGameOfLife width height cellDim = 
-  let
-      automaton = buildAutomaton width height cellDim
-      
-      r1 neighborCount cell =
-        if neighborCount < 2 then Nothing else Just cell
-
-      r2 neighborCount cell =
-        if neighborCount == 2 || neighborCount == 3 then Just cell else Nothing
-
-      r3 neighborCount cell =
-        if neighborCount > 3 then Nothing else Just cell
-
-      r4 neighborCount cell =
-        if neighborCount == 3 then Just cell else Nothing
-  in
-    { automaton | liveRules = [ r1, r2, r3 ]
-                , deadRules = [ r4 ] }
-
 
 
 -- SUBSCRIPTIONS
