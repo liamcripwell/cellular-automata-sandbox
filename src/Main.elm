7937be74043aff89ca6ed3794f3549020e9d3e04
liam <ljcripwell@gmail.com>
2019-02-11 19:50:33 +0100

    minor edits

--- src/Main.elm
+++ src/Main.elm
@@ -53,7 +53,6 @@ cellSize = 19 -- Note: cell's internal size will be (cellSize-1)*(cellSize-1)
 
 type Msg
   = Step Time.Posix
-  | NewState Automata.Automaton -- (List (Int, Int))
   | Pause Bool
 
 
@@ -72,11 +71,6 @@ update msg model =
         , Cmd.none
         )
 
-    NewState newAutomatonState ->
-      ( { model | automaton  = newAutomatonState }
-      , Cmd.none
-      )
-
     Pause newState ->
       ( { model | paused = newState }
       , Cmd.none
@@ -88,64 +82,6 @@ randomCell = Random.pair (Random.int 0 (gridWidth-1)) (Random.int 0 (gridHeight-
 
 randomCells : Int -> Random.Generator (List (Int, Int))
 randomCells n = Random.list n randomCell
-
-cartesian : List a -> List b -> List (a,b)
-cartesian xs ys =
-  List.concatMap
-    ( \x -> List.map ( \y -> (x, y) ) ys )
-    xs
-
-allNeighbors : List (Int, Int) -> (Int, Int) -> List (Int, Int)
-allNeighbors state (x, y) = 
-  cartesian (List.range (x-1) (x+1)) (List.range (y-1) (y+1))
-
-liveneighbors : List (Int, Int) -> (Int, Int) -> List (Int, Int)
-liveneighbors state (x, y) = 
-  allNeighbors state (x, y)
-  |> List.filter (\a -> List.member a state)
-
-rule1 : Int -> (Int, Int) -> Maybe (Int, Int)
-rule1 neighborCount cell =
-  if neighborCount < 2 then Nothing else Just cell
-
-rule2 : Int -> (Int, Int) -> Maybe (Int, Int)
-rule2 neighborCount cell =
-  if neighborCount == 2 || neighborCount == 3 then Just cell else Nothing
-
-rule3 : Int -> (Int, Int) -> Maybe (Int, Int)
-rule3 neighborCount cell =
-  if neighborCount > 3 then Nothing else Just cell
-
-rule4 : Int -> (Int, Int) -> List (Int, Int)
-rule4 neighborCount cell =
-  if neighborCount == 3 then [cell] else []
-
-gameOfLife : List (Int, Int) -> (Int, Int) -> List (Int, Int)
-gameOfLife liveCells cell =
-  let
-    neighbors = liveneighbors liveCells cell
-    neighborCount = (List.length neighbors) - 1
-  in
-    case rule1 neighborCount cell of
-      Nothing -> []
-      Just x -> 
-        case rule2 neighborCount cell of
-          Nothing -> []
-          Just y ->
-            case rule3 neighborCount cell of
-                Nothing -> []
-                Just z -> [z]
-
-gameOfDeath : List (Int, Int) -> (Int, Int) -> List (Int, Int)
-gameOfDeath liveCells cell =
-    let
-        neighbors = liveneighbors liveCells cell
-        neighborCount = (List.length neighbors)
-        isLive = List.member cell liveCells
-    in
-      if isLive then []
-      else rule4 neighborCount cell
-
 
 
 
